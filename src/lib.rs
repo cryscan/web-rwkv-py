8d551b3c2291a7fbd7ef5250ff4fa76aee1c16c5
cryscan <cryscan@umich.edu>
2023-12-14 14:10:37 +0800

    Inference works.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,122 @@
-use pyo3::prelude::*;
+use std::{fs::File, path::PathBuf};
+
+use anyhow::Result;
+use memmap2::Mmap;
+use pyo3::{exceptions::PyValueError, prelude::*};
+use web_rwkv::{
+    context::{Context, ContextBuilder, Instance},
+    model::loader::Loader,
+    wgpu,
+};
 
 mod v5;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+async fn create_context() -> Result<Context> {
+    let instance = Instance::new();
+    let adapter = instance
+        .adapter(wgpu::PowerPreference::HighPerformance)
+        .await?;
+    let context = ContextBuilder::new(adapter)
+        .with_default_pipelines()
+        .build()
+        .await?;
+    println!("{:#?}", context.adapter.get_info());
+    Ok(context)
 }
 
-/// A Python module implemented in Rust.
+#[pyclass]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ModelVersion {
+    V4,
+    V5,
+    V6,
+}
+
+impl From<web_rwkv::model::ModelVersion> for ModelVersion {
+    fn from(value: web_rwkv::model::ModelVersion) -> Self {
+        match value {
+            web_rwkv::model::ModelVersion::V4 => Self::V4,
+            web_rwkv::model::ModelVersion::V5 => Self::V5,
+            web_rwkv::model::ModelVersion::V6 => Self::V6,
+        }
+    }
+}
+
+#[pymethods]
+impl ModelVersion {
+    #[pyo3(name = "__str__")]
+    pub fn str(&self) -> String {
+        format!("{:?}", self)
+    }
+}
+
+#[pyclass]
+#[derive(Debug, Clone, Copy)]
+pub struct ModelInfo {
+    #[pyo3(get)]
+    pub version: ModelVersion,
+    #[pyo3(get)]
+    pub num_layer: usize,
+    #[pyo3(get)]
+    pub num_emb: usize,
+    #[pyo3(get)]
+    pub num_hidden: usize,
+    #[pyo3(get)]
+    pub num_vocab: usize,
+    #[pyo3(get)]
+    pub num_head: usize,
+}
+
+impl From<web_rwkv::model::ModelInfo> for ModelInfo {
+    fn from(value: web_rwkv::model::ModelInfo) -> Self {
+        let web_rwkv::model::ModelInfo {
+            version,
+            num_layer,
+            num_emb,
+            num_hidden,
+            num_vocab,
+            num_head,
+        } = value;
+        Self {
+            version: version.into(),
+            num_layer,
+            num_emb,
+            num_hidden,
+            num_vocab,
+            num_head,
+        }
+    }
+}
+
+#[pymethods]
+impl ModelInfo {
+    #[pyo3(name = "__str__")]
+    pub fn str(&self) -> String {
+        format!("{:#?}", self)
+    }
+}
+
+#[pyfunction]
+fn peek_info(file: PathBuf) -> PyResult<ModelInfo> {
+    let info = || -> Result<ModelInfo> {
+        let file = File::open(file)?;
+        let map = unsafe { Mmap::map(&file)? };
+        Ok(Loader::info(&map)?.into())
+    };
+    info().map_err(|err| PyValueError::new_err(err.to_string()))
+}
+
 #[pymodule]
 fn web_rwkv_py(py: Python, module: &PyModule) -> PyResult<()> {
-    module.add_function(wrap_pyfunction!(sum_as_string, module)?)?;
+    module.add_class::<ModelVersion>()?;
+    module.add_class::<ModelInfo>()?;
+    module.add_function(wrap_pyfunction!(peek_info, module)?)?;
 
     let module_v5 = PyModule::new(py, "v5")?;
     module_v5.add_class::<v5::Model>()?;
     module_v5.add_class::<v5::ModelState>()?;
     module_v5.add_class::<v5::BackedState>()?;
+    module_v5.add_function(wrap_pyfunction!(v5::run_one, module_v5)?)?;
     module.add_submodule(module_v5)?;
 
     Ok(())
--- src/v5.rs
+++ src/v5.rs
@@ -5,37 +5,32 @@ use itertools::Itertools;
 use memmap2::Mmap;
 use pyo3::{exceptions::PyValueError, prelude::*};
 use web_rwkv::{
-    context::{Context, ContextBuilder, Instance},
-    model::{loader::Loader, v5, ModelBuilder, ModelVersion, Quant},
-    wgpu,
+    context::Context,
+    model::{
+        loader::Loader, run::ModelRun as _, v5, Lora, ModelBase as _, ModelBuilder,
+        ModelState as _, ModelVersion, Quant, StateBuilder,
+    },
 };
+use web_rwkv_derive::Deref;
+
+use crate::create_context;
 
 #[pyclass]
+#[derive(Debug, Deref, Clone)]
 pub struct Model(Arc<v5::Model<'static>>);
 
 #[pyclass]
+#[derive(Debug, Deref, Clone)]
 pub struct ModelState(Arc<v5::ModelState>);
 
 #[pyclass]
-pub struct BackedState(Arc<v5::BackedState>);
-
-async fn create_context() -> Result<Context> {
-    let instance = Instance::new();
-    let adapter = instance
-        .adapter(wgpu::PowerPreference::HighPerformance)
-        .await?;
-    let context = ContextBuilder::new(adapter)
-        .with_default_pipelines()
-        .build()
-        .await?;
-    println!("{:#?}", context.adapter.get_info());
-    Ok(context)
-}
+#[derive(Debug, Deref, Clone)]
+pub struct BackedState(v5::BackedState);
 
 fn load_model(
     context: &Context,
     data: &[u8],
-    // lora: Option<PathBuf>,
+    lora: Option<PathBuf>,
     quant: Option<usize>,
     quant_nf4: Option<usize>,
     turbo: bool,
@@ -50,20 +45,19 @@ fn load_model(
     let model = ModelBuilder::new(context, data)
         .with_quant(quant)
         .with_turbo(turbo);
-    model.build()
-    // match lora {
-    //     Some(lora) => {
-    //         let file = File::open(lora)?;
-    //         let map = unsafe { Mmap::map(&file)? };
-    //         model
-    //             .add_lora(Lora {
-    //                 data: map.to_vec(),
-    //                 blend: Default::default(),
-    //             })
-    //             .build()
-    //     }
-    //     None => model.build(),
-    // }
+    match lora {
+        Some(lora) => {
+            let file = File::open(lora)?;
+            let map = unsafe { Mmap::map(&file)? };
+            model
+                .add_lora(Lora {
+                    data: map.to_vec(),
+                    blend: Default::default(),
+                })
+                .build()
+        }
+        None => model.build(),
+    }
 }
 
 #[pymethods]
@@ -72,6 +66,7 @@ impl Model {
     pub fn new(
         file: PathBuf,
         turbo: bool,
+        lora: Option<PathBuf>,
         quant: Option<usize>,
         quant_nf4: Option<usize>,
     ) -> PyResult<Self> {
@@ -81,9 +76,10 @@ impl Model {
             let map = unsafe { Mmap::map(&file)? };
 
             let info = Loader::info(&map)?;
+            println!("{:#?}", info);
             match info.version {
                 ModelVersion::V5 => {
-                    anyhow::Ok(load_model(&context, &map, quant, quant_nf4, turbo)?)
+                    anyhow::Ok(load_model(&context, &map, lora, quant, quant_nf4, turbo)?)
                 }
                 version => bail!(
                     "model version {:?} is incorrect, should be {:?}",
@@ -101,7 +97,61 @@ impl Model {
 #[pymethods]
 impl ModelState {
     #[new]
-    pub fn new(_model: &Model) -> Self {
-        todo!()
+    pub fn new(model: &Model, batch: usize) -> Self {
+        let context = model.context();
+        let info = model.info();
+        Self(
+            StateBuilder::new(context, info)
+                .with_max_batch(batch)
+                .build::<v5::ModelState>()
+                .into(),
+        )
     }
+
+    pub fn load(&self, backed: &BackedState) -> PyResult<()> {
+        self.0
+            .load(backed)
+            .map_err(|err| PyValueError::new_err(err.to_string()))?;
+        Ok(())
+    }
+
+    pub fn load_batch(&self, backed: &BackedState, batch: usize) -> PyResult<()> {
+        self.0
+            .load_batch(backed, batch)
+            .map_err(|err| PyValueError::new_err(err.to_string()))?;
+        Ok(())
+    }
+
+    pub fn back(&self) -> BackedState {
+        let back = pollster::block_on(self.0.back());
+        BackedState(back)
+    }
+
+    pub fn back_batch(&self, batch: usize) -> PyResult<BackedState> {
+        let back = pollster::block_on(self.0.back_batch(batch))
+            .map_err(|err| PyValueError::new_err(err.to_string()))?;
+        Ok(BackedState(back))
+    }
+}
+
+fn run_one_internal(model: Model, state: ModelState, tokens: Vec<u16>) -> Result<Vec<f32>> {
+    if state.max_batch() != 1 {
+        bail!("state batch size must be 1")
+    }
+    if tokens.is_empty() {
+        bail!("prompt cannot be empty")
+    }
+
+    let mut tokens = vec![tokens];
+    let mut logits = vec![None];
+
+    while logits[0].is_none() {
+        logits = pollster::block_on(model.run(&mut tokens, &state.0))?;
+    }
+    Ok(logits[0].clone().unwrap())
+}
+
+#[pyfunction]
+pub fn run_one(model: Model, state: ModelState, tokens: Vec<u16>) -> PyResult<Vec<f32>> {
+    run_one_internal(model, state, tokens).map_err(|err| PyValueError::new_err(err.to_string()))
 }
